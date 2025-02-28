615ae8b7855fcde5860168d052d3951ca0ad2cec
tnagasim <40717403+tnagasim@users.noreply.github.com>
2025-02-28 15:28:26 +0900

    2.2 アプリを起動する

--- apps/app.py
+++ apps/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 from apps.config import config
 from apps.auth import views as auth_views
+from apps.crud import views as crud_views
 
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+login_manager = LoginManager()
+login_manager.login_view = "auth.signup"
+login_manager.login_message = ""
 
 
 def create_app(config_key):
@@ -16,5 +21,7 @@ def create_app(config_key):
     app.config.from_object(config[config_key])
     db.init_app(app)
     Migrate(app, db)
+    # login_manager.init_app(app)
     app.register_blueprint(auth_views.auth, url_prefix="/auth")
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
     return app
