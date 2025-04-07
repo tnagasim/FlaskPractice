1555a062f98a0d2a3e73fdaf28ce7198db3a7aa3
tnagasim <40717403+tnagasim@users.noreply.github.com>
2025-04-07 17:25:28 +0900

    6.4 ログイン画面のエンドポイントを更新する

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -38,7 +38,7 @@ def signup():
         login_user(user)
         next_ = request.args.get("next")
         if next_ is None or not next_.startswith("/"):
-            next_ = url_for("crud.users")
+            next_ = url_for("detector.index")
         return redirect(next_)
     return render_template("auth/signup.html", form=form)
 
@@ -50,7 +50,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
-            return redirect(url_for("crud.users"))
+            return redirect(url_for("detector.index"))
         flash("メールアドレスかパスワードが不正です")
     return render_template("auth/login.html", form=form)
 
