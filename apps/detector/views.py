1704f77eb68c8958dfff4defdf7438e9a109ee43
tnagasim <40717403+tnagasim@users.noreply.github.com>
2025-04-12 10:19:21 +0900

    8.4 物体検知機能のエンドポイントを作成する

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,6 +1,12 @@
+import random
 import uuid
 from pathlib import Path
 
+import cv2
+import numpy as np
+import PIL
+import torch
+import torchvision
 from flask import (
     Blueprint,
     current_app,
@@ -8,13 +14,15 @@ from flask import (
     render_template,
     send_from_directory,
     url_for,
+    flash,
 )
 from flask_login import current_user, login_required
+from sqlalchemy.exc import SQLAlchemyError
 
 from apps.app import db
 from apps.crud.models import User
 from apps.detector.forms import UploadImageForm
-from apps.detector.models import UserImage
+from apps.detector.models import UserImage, UserImageTag
 
 
 dt = Blueprint("detector", __name__, template_folder="templates")
@@ -55,3 +63,109 @@ def upload_image():
         db.session.commit()
         return redirect(url_for("detector.index"))
     return render_template("detector/upload.html", form=form)
+
+
+@dt.route("/detect/<string:image_id>", methods=["POST"])
+@login_required
+def detect(image_id):
+    user_image = (
+        db.session.query(UserImage).filter(UserImage.id == image_id).first()
+    )
+    if user_image is None:
+        flash("物体検知対象の画像が存在しません。")
+        return redirect(url_for("detector.index"))
+    target_image_path = Path(
+        current_app.config["UPLOAD_FOLDER"], user_image.image_path
+    )
+    tags, detected_image_file_name = exec_detect(target_image_path)
+    try:
+        save_detected_image_tags(user_image, tags, detected_image_file_name)
+    except SQLAlchemyError as e:
+        flash("物体検知処理でエラーが発生しました。")
+        db.session.rollback()
+        current_app.logger.error(e)
+        return redirect(url_for("detector.index"))
+    return redirect(url_for("detector.index"))
+
+
+def make_color(labels):
+    colors = [[random.randint(0, 225) for _ in range(3)] for _ in labels]
+    color = random.choice(colors)
+    return color
+
+
+def make_line(result_image):
+    line = round(0.002 * max(result_image.shape[0: 2])) + 1
+    return line
+
+
+def draw_lines(c1, c2, result_image, line, color):
+    cv2.rectangle(result_image, c1, c2, color, thickness=line)
+    return cv2
+
+
+def draw_texts(result_image, line, c1, cv2, color, labels, label):
+    display_txt = f"{labels[label]}"
+    font = max(line - 1, 1)
+    t_size = cv2.getTextSize(
+        display_txt, 0, fontScale=line / 3, thickness=font
+    )[0]
+    c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+    cv2.rectangle(result_image, c1, c2, color, -1)
+    cv2.putText(
+        result_image,
+        display_txt,
+        (c1[0], c1[1] - 2),
+        0,
+        line / 3,
+        [255, 255, 255],
+        thickness=font,
+        lineType=cv2.LINE_AA,
+    )
+    return cv2
+
+
+def exec_detect(target_image_path):
+    labels = current_app.config["LABELS"]
+    image = PIL.Image.open(target_image_path)
+    image_tensor = torchvision.transforms.functional.to_tensor(image)
+    model = torch.load(
+        Path(current_app.root_path, "detector", "model.pt"),
+        weights_only=False,
+    )
+    model = model.eval()
+    output = model([image_tensor])[0]
+    tags = []
+    result_image = np.array(image.copy())
+    for box, label, score in zip(
+        output["boxes"], output["labels"], output["scores"]
+    ):
+        if score > 0.5 and labels[label] not in tags:
+            color = make_color(labels)
+            line = make_line(result_image)
+            c1 = (int(box[0]), int(box[1]))
+            c2 = (int(box[2]), int(box[3]))
+            cv2 = draw_lines(c1, c2, result_image, line, color)
+            cv2 = draw_texts(result_image, line, c1, cv2, color, labels, label)
+            tags.append(labels[label])
+
+    detected_image_file_name = str(uuid.uuid4()) + ".jpg"
+    detected_image_file_path = str(
+        Path(current_app.config["UPLOAD_FOLDER"], detected_image_file_name)
+    )
+    cv2.imwrite(
+        detected_image_file_path, cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR)
+    )
+    return tags, detected_image_file_name
+
+
+def save_detected_image_tags(user_image, tags, detected_image_file_name):
+    user_image.image_path = detected_image_file_name
+    user_image.is_detected = True
+    db.session.add(user_image)
+    for tag in tags:
+        user_image_tag = UserImageTag(
+            user_image_id=user_image.id, tag_name=tag
+        )
+        db.session.add(user_image_tag)
+    db.session.commit()
